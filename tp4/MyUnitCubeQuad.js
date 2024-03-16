ce4da7373f5682d2fff8b6f721c121e0c2e2eded
Afonso Castro Vaz Osorio <up202108700@fe.up.pt>
2024-03-16 17:35:32 +0000

    fix

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -41,7 +41,7 @@ export class MyUnitCubeQuad extends CGFobject {
 		this.scene.pushMatrix();
 		this.scene.translate(0,-0.5,0);
 		this.scene.rotate(Math.PI,1,0,0);
-		this.quad.updateTexCoords([0,0,1,0,0,1,1,1]);
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 		this.quad.display();
 		this.scene.popMatrix();
 
@@ -49,7 +49,7 @@ export class MyUnitCubeQuad extends CGFobject {
 		this.material.apply();
 		this.scene.pushMatrix();
 		this.scene.translate(0,0.5,0);
-		this.quad.updateTexCoords([0,0,1,0,0,1,1,1])
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 		this.quad.display();
 		this.scene.popMatrix();
 
@@ -59,6 +59,7 @@ export class MyUnitCubeQuad extends CGFobject {
 		this.scene.translate(0,0,0.5);
 		this.scene.rotate(Math.PI/2,1,0,0);
 		this.quad.updateTexCoords([0,0,1,0,0,1,1,1]);
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 		this.quad.display();
 		this.scene.popMatrix();
 
@@ -68,6 +69,7 @@ export class MyUnitCubeQuad extends CGFobject {
 		this.scene.translate(0,0,-0.5);
 		this.scene.rotate(-Math.PI/2,1,0,0);
 		this.quad.updateTexCoords([1,1,0,1,1,0,0,0]);
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 		this.quad.display();
 		this.scene.popMatrix();
 
@@ -76,7 +78,8 @@ export class MyUnitCubeQuad extends CGFobject {
 		this.scene.pushMatrix();
 		this.scene.translate(-0.5,0,0);
 		this.scene.rotate(Math.PI/2,0,0,1);
-		this.quad.updateTexCoords([0,1,0,0,1,1,1,0])
+		this.quad.updateTexCoords([0,1,0,0,1,1,1,0]);
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 		this.quad.display();
 		this.scene.popMatrix();
 
@@ -86,6 +89,7 @@ export class MyUnitCubeQuad extends CGFobject {
 		this.scene.translate(0.5,0,0);
 		this.scene.rotate(-Math.PI/2,0,0,1);
 		this.quad.updateTexCoords([1,0,1,1,0,0,0,1]);
+		this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_MAG_FILTER, this.scene.gl.NEAREST);
 		this.quad.display();
 		this.scene.popMatrix();
 
