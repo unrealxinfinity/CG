4bff2b56b72b39237ae182e272f845e6ab26df20
HaoChang Fu <up202108730@edu.fe.up.pt>
2024-03-16 17:23:00 +0000

    some fixes

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -41,6 +41,7 @@ export class MyUnitCubeQuad extends CGFobject {
 		this.scene.pushMatrix();
 		this.scene.translate(0,-0.5,0);
 		this.scene.rotate(Math.PI,1,0,0);
+		this.quad.updateTexCoords([0,0,1,0,0,1,1,1]);
 		this.quad.display();
 		this.scene.popMatrix();
 
@@ -48,6 +49,7 @@ export class MyUnitCubeQuad extends CGFobject {
 		this.material.apply();
 		this.scene.pushMatrix();
 		this.scene.translate(0,0.5,0);
+		this.quad.updateTexCoords([0,0,1,0,0,1,1,1])
 		this.quad.display();
 		this.scene.popMatrix();
 
