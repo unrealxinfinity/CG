cf4697ca377f7e7975e3b7ffd3cdd8052111e277
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-03-18 09:39:57 +0000

    grayscale

--- tp5/ShaderScene.js
+++ tp5/ShaderScene.js
@@ -89,7 +89,8 @@ export class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture3anim.vert", "shaders/texture3anim.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/sepia.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
-			new CGFshader(this.gl, "shaders/twocolors.vert", "shaders/twocolors.frag")
+			new CGFshader(this.gl, "shaders/twocolors.vert", "shaders/twocolors.frag"),
+			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/grayscale.frag")
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
@@ -111,7 +112,8 @@ export class ShaderScene extends CGFscene {
 			'Animation example': 6,
 			'Sepia': 7,
 			'Convolution': 8,
-			'Two colors': 9
+			'Two colors': 9,
+			'Grayscale': 10
 		};
 
 		// shader code panels references
