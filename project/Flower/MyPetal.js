e94c3c624b3ac68990ee1100d0ef4ff28e3ae1c9
Afonso Castro Vaz Osorio <up202108700@fe.up.pt>
2024-04-13 15:30:11 +0100

    indentation

--- project/Flower/MyPetal.js
+++ project/Flower/MyPetal.js
@@ -13,17 +13,17 @@ export class MyPetal extends CGFobject {
 	}
 	
 	display() {
-        this.scene.pushMatrix();
-        this.scene.scale(0.5, 1, 1);
-        this.scene.pushMatrix();
-        this.scene.rotate(Math.PI, 0, 0, 1);
-        this.scene.translate(0, -1, 0);
-        this.triangle.display();
-        this.scene.popMatrix(); 
-        this.scene.translate(0, 1, 0);
-        this.scene.rotate(this.angle, 1, 0, 0);
-        this.triangle.display();
-        this.scene.popMatrix();
+                this.scene.pushMatrix();
+                this.scene.scale(0.5, 1, 1);
+                this.scene.pushMatrix();
+                this.scene.rotate(Math.PI, 0, 0, 1);
+                this.scene.translate(0, -1, 0);
+                this.triangle.display();
+                this.scene.popMatrix(); 
+                this.scene.translate(0, 1, 0);
+                this.scene.rotate(this.angle, 1, 0, 0);
+                this.triangle.display();
+                this.scene.popMatrix();
 	}
 
         enableNormalViz() {
