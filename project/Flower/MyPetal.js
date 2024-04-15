0a7cade78af016be7a2e9f9f2942bea8e7c863df
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-04-15 09:40:20 +0100

    petals scale with size

--- project/Flower/MyPetal.js
+++ project/Flower/MyPetal.js
@@ -6,23 +6,27 @@ import { MyTriangle } from '../Objects/MyTriangle.js';
  * @param scene - Reference to MyScene object
  */
 export class MyPetal extends CGFobject {
-	constructor(scene) {
+	constructor(scene, innerRadius, outerRadius) {
 		super(scene);
+                this.innerRadius = innerRadius;
+                this.outerRadius = outerRadius;
+                this.diff = (this.outerRadius-this.innerRadius)/2;
 		this.triangle = new MyTriangle(scene);
 	}
 	
 	display(angle, texCoords) {
                 this.scene.pushMatrix();
-                this.scene.scale(0.5, 1, 1);
                 this.scene.pushMatrix();
                 this.scene.rotate(Math.PI, 0, 0, 1);
                 this.scene.translate(0, -1, 0);
                 this.triangle.updateTexCoords(texCoords[0]);
+                this.scene.scale(0.5, 1, this.diff);
                 this.triangle.display();
                 this.scene.popMatrix(); 
                 this.scene.translate(0, 1, 0);
                 this.scene.rotate(angle, 1, 0, 0);
                 this.triangle.updateTexCoords(texCoords[1]);
+                this.scene.scale(0.5, 1, this.diff);
                 this.triangle.display();
                 this.scene.popMatrix();
 	}
