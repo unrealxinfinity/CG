6a182ac2ee1baa80d7abf35673dc882c6320aca2
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-04-29 09:13:45 +0100

    pollen texture

--- project/Objects/MyPollen.js
+++ project/Objects/MyPollen.js
@@ -1,4 +1,4 @@
-import { CGFobject } from '../../lib/CGF.js';
+import { CGFappearance, CGFobject, CGFtexture } from '../../lib/CGF.js';
 import { MySphere } from './MySphere.js';
 /**
  * MySphere
@@ -11,10 +11,22 @@ export class MyPollen extends CGFobject {
 		super(scene);
 
         this.sphere = new MySphere(scene, 10, 10, false, false, 1, 1.5, 1);
+        this.initMaterial();
 	}
+
+    initMaterial() {
+        this.appearance = new CGFappearance(this.scene);
+        const texture = new CGFtexture(this.scene, "images/pollen.jpg");
+        this.appearance.setTexture(texture);
+        this.appearance.setTextureWrap('REPEAT', 'REPEAT');
+        this.appearance.setAmbient(1,1,1,1);
+        this.appearance.setDiffuse(1,1,1,1);
+        this.appearance.setSpecular(0,0,0,1);
+    }
 
     display() {
         this.scene.pushMatrix();
+        this.appearance.apply();
         this.sphere.display();
         this.scene.popMatrix();
     }
