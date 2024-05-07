e0312fc24d03e7bf80504bed90663924e8b6f984
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-05-07 19:12:52 +0100

    blade of grass

---
+++ project/Objects/MyGrassBlade.js
@@ -0,0 +1,49 @@
+import {CGFobject} from '../../lib/CGF.js';
+/**
+* MyCone
+* @constructor
+ * @param scene - Reference to MyScene object
+ * @param slices - number of divisions around the Y axis
+*/
+export class MyGrassBlade extends CGFobject {
+    constructor(scene, slices) {
+        super(scene);
+        this.slices = slices;
+        this.offsetDecay = 0.9;
+        this.offset = 0.1;
+        this.initBuffers();
+    }
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.direction = Math.floor(Math.random()*2);
+        if (this.direction == 0) this.direction = -1;
+
+        this.vertices.push(-0.25,0,0,0.25,0,0);
+        this.normals.push(0,0,1,0,0,1);
+        let center = 0;
+        let amplitude = 0.25;
+        let currIndex = 0;
+        for (let i = 1; i < this.slices; i++) {
+            const sliceOffset = Math.random()*this.offset*this.direction;
+            center += sliceOffset;
+            amplitude *= 0.75;
+            const y = i/this.slices;
+            this.vertices.push(center-amplitude,y,0,center+amplitude,y,0);
+            this.normals.push(0,0,1,0,0,1);
+            this.indices.push(currIndex,currIndex+1,currIndex+2,currIndex+2,currIndex+1,currIndex+3);
+            currIndex += 2;
+            this.offset *= this.offsetDecay;
+        }
+
+        this.vertices.push(center,1,0);
+        this.normals.push(0,0,1);
+        this.indices.push(currIndex,currIndex+1,currIndex+2);
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+}
+
+
